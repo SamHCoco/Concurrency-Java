bda8bbecb25a1ee81797d65bf0bb6013a421cfb9
Sam Coco <Samuel.HenriAC@outlook.com>
2019-10-23 12:58:05 +0100

    run method edited

--- src/concurrent/RunnableThread.java
+++ src/concurrent/RunnableThread.java
@@ -3,10 +3,8 @@ package concurrent;
 import java.lang.Runnable;
 
 public class RunnableThread implements Runnable {
-
     private static final String threadName = "RUNNABLE-THREAD";
 
-    @Override
     public void run(){
         System.out.println("This message is from: " + threadName);
     }
