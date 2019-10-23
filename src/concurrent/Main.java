dd87e6fa7ed5492ff66ae5aeba1bd6ecc189f2d6
Sam Coco <Samuel.HenriAC@outlook.com>
2019-10-23 12:59:15 +0100

    2 approaches to running threads added

--- src/concurrent/Main.java
+++ src/concurrent/Main.java
@@ -5,9 +5,13 @@ public class Main {
 
     public static void main(String[] args) {
     	System.out.println("This message is from: " + Thread.currentThread().getName().toUpperCase());
-	    Thread thread1 = new Thread1();
+
+    	Thread thread1 = new Thread1();
 	    Thread thread2 = new Thread2();
+	    Thread runnableThread = new Thread(new RunnableThread());
+
 	    thread1.start();
 	    thread2.start();
+	    runnableThread.start();
     }
 }
