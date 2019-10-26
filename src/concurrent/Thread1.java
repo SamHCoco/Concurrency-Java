cf989611144ab552dd03f12d1cb7beab468f5f6b
Sam Coco <Samuel.HenriAC@outlook.com>
2019-10-26 23:44:14 +0100

    Constructor added

--- src/concurrent/Thread1.java
+++ src/concurrent/Thread1.java
@@ -4,9 +4,12 @@ import java.lang.Thread;
 public class Thread1 extends Thread {
     private final static String threadName = "THREAD-1";
 
+    public Thread1(){
+        this.setName(threadName);
+    }
+
     @Override
     public void run(){
-        this.setName(threadName);
         System.out.println("This message is from: " + this.getName());
     }
 }
